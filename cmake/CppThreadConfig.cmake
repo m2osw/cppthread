6ead990f856f2a21e5fcf8f02dd45f06b9d915ad
AlexisWilke <alexis@m2osw.com>
2019-09-10 09:19:06 -0700

    Fixed the name of the output: it is plural.

--- cmake/CppThreadConfig.cmake
+++ cmake/CppThreadConfig.cmake
@@ -3,8 +3,8 @@
 # Once done this will define
 #
 # CPPTHREAD_FOUND        - System has CppThread
-# CPPTHREAD_INCLUDE_DIR  - The CppThread include directories
-# CPPTHREAD_LIBRARY      - The libraries needed to use CppThread (none)
+# CPPTHREAD_INCLUDE_DIRS - The CppThread include directories
+# CPPTHREAD_LIBRARIES    - The libraries needed to use CppThread (none)
 # CPPTHREAD_DEFINITIONS  - Compiler switches required for using CppThread (none)
 #
 # License:
@@ -28,18 +28,39 @@
 #       Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
-find_path( CPPTHREAD_INCLUDE_DIR cppthread/thread.h
-		   PATHS $ENV{CPPTHREAD_INCLUDE_DIR}
-		 )
-find_library( CPPTHREAD_LIBRARY cppthread
-		   PATHS $ENV{CPPTHREAD_LIBRARY}
-		 )
-mark_as_advanced( CPPTHREAD_INCLUDE_DIR CPPTHREAD_LIBRARY )
+find_path(
+    CPPTHREAD_INCLUDE_DIR
+        cppthread/thread.h
 
-set( CPPTHREAD_INCLUDE_DIRS ${CPPTHREAD_INCLUDE_DIR} )
-set( CPPTHREAD_LIBRARIES    ${CPPTHREAD_LIBRARY}     )
+    PATHS
+        $ENV{CPPTHREAD_INCLUDE_DIR}
+)
+
+find_library(
+    CPPTHREAD_LIBRARY
+        cppthread
+
+    PATHS
+        $ENV{CPPTHREAD_LIBRARY}
+)
+
+mark_as_advanced(
+    CPPTHREAD_INCLUDE_DIR
+    CPPTHREAD_LIBRARY
+)
+
+set(CPPTHREAD_INCLUDE_DIRS ${CPPTHREAD_INCLUDE_DIR})
+set(CPPTHREAD_LIBRARIES    ${CPPTHREAD_LIBRARY}    )
 
 include( FindPackageHandleStandardArgs )
+
 # handle the QUIETLY and REQUIRED arguments and set CPPTHREAD_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args( CppThread DEFAULT_MSG CPPTHREAD_INCLUDE_DIR CPPTHREAD_LIBRARY )
+find_package_handle_standard_args(
+    CppThread
+    DEFAULT_MSG
+    CPPTHREAD_INCLUDE_DIR
+    CPPTHREAD_LIBRARY
+)
+
+# vim: ts=4 sw=4 et
