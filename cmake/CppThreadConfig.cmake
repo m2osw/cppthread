4cb28fe707ce50cbb91439c24b17580e91299683
AlexisWilke <alexis@m2osw.com>
2022-05-17 18:03:40 -0700

    Cleaned up the cmake file.

--- cmake/CppThreadConfig.cmake
+++ cmake/CppThreadConfig.cmake
@@ -1,37 +1,36 @@
-# The following variables are defined with the C++ Thread library parameters:
+# - Find C++ Thread
 #
 # CPPTHREAD_FOUND        - System has CppThread
 # CPPTHREAD_INCLUDE_DIRS - The CppThread include directories
-# CPPTHREAD_LIBRARIES    - The libraries needed to use CppThread (none)
-# CPPTHREAD_DEFINITIONS  - Compiler switches required for using CppThread (none)
+# CPPTHREAD_LIBRARIES    - The libraries needed to use CppThread
+# CPPTHREAD_DEFINITIONS  - Compiler switches required for using CppThread
 #
 # License:
-#   Copyright (c) 2013-2022  Made to Order Software Corp.  All Rights Reserved
 #
-#   https://snapwebsites.org/
-#   contact@m2osw.com
-# 
-#   This program is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; either version 2 of the License, or
-#   (at your option) any later version.
+# Copyright (c) 2013-2022  Made to Order Software Corp.  All Rights Reserved
 #
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
+# https://snapwebsites.org/project/cppthread
+# contact@m2osw.com
 #
-#   You should have received a copy of the GNU General Public License along
-#   with this program; if not, write to the Free Software Foundation, Inc.,
-#   51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 find_path(
     CPPTHREAD_INCLUDE_DIR
         cppthread/thread.h
 
     PATHS
-        $ENV{CPPTHREAD_INCLUDE_DIR}
+        ENV CPPTHREAD_INCLUDE_DIR
 )
 
 find_library(
@@ -39,7 +38,8 @@ find_library(
         cppthread
 
     PATHS
-        $ENV{CPPTHREAD_LIBRARY}
+        ${CPPTHREAD_LIBRARY_DIR}
+        ENV CPPTHREAD_LIBRARY
 )
 
 mark_as_advanced(
@@ -48,17 +48,15 @@ mark_as_advanced(
 )
 
 set(CPPTHREAD_INCLUDE_DIRS ${CPPTHREAD_INCLUDE_DIR})
-set(CPPTHREAD_LIBRARIES    ${CPPTHREAD_LIBRARY}    )
+set(CPPTHREAD_LIBRARIES    ${CPPTHREAD_LIBRARY})
 
-include( FindPackageHandleStandardArgs )
 
-# handle the QUIETLY and REQUIRED arguments and set CPPTHREAD_FOUND to TRUE
-# if all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
     CppThread
-    DEFAULT_MSG
-    CPPTHREAD_INCLUDE_DIR
-    CPPTHREAD_LIBRARY
+    REQUIRED_VARS
+        CPPTHREAD_INCLUDE_DIR
+        CPPTHREAD_LIBRARY
 )
 
 # vim: ts=4 sw=4 et
