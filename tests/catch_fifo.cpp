8e96872b72e8e0484d6c50caf2d8878422636cd5
AlexisWilke <alexis@m2osw.com>
2025-06-29 13:19:53 -0700

    Tiny clean up.

--- tests/catch_fifo.cpp
+++ tests/catch_fifo.cpp
@@ -17,7 +17,7 @@
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-// cppthread lib
+// cppthread
 //
 #include    <cppthread/fifo.h>
 
@@ -37,12 +37,12 @@
 #include    "catch_main.h"
 
 
-// snapdev lib
+// snapdev
 //
 #include    <snapdev/not_reached.h>
 
 
-// C lib
+// C
 //
 #include    <unistd.h>
 
